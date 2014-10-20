14f5270e5b6c5cac4c91c71f0c9034a861ec4a09
Lucas Santos <l-santos@live.com>
2014-10-20 17:59:23 -0300

    Identei o codigo

--- Clinica_Odonto/src/Users/Cliente.java
+++ Clinica_Odonto/src/Users/Cliente.java
@@ -1,8 +1,4 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
+
 package Users;
 
 /**
@@ -14,21 +10,27 @@ public class Cliente {
     private String nome;
     private String telefone;
     
-    //TODO: Adicionar gets e sets para o Cliente
-    public String getCpf(){
+    public String getCpf() {
         return this.cpf;
     }
     
-    public String setCpf(String _cpf){
+    public void setCpf(String _cpf) {
         this.cpf = _cpf;
     }
     
-     public String getNome(){
-        return this.cpf;
+     public String getNome() {
+        return this.nome;
     }
     
-    public String setNome(String _nome)
-    {
-        this.cpf = _cpf;
+    public void setNome(String _nome) {
+        this.nome = _nome;
     }
+    
+    public String getTelefone() {
+        return this.telefone;
+    }
+    
+    public void setTelefone(String _telefone) {
+        this.telefone = _telefone;
+    }   
 }
